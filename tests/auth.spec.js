c2e1fcbdb8e1162270985cf12e66a67ec55e5222
Your Name <you@example.com>
2023-08-31 21:57:14 +0200

    New Project Update

---
+++ tests/auth.spec.js
@@ -0,0 +1,13 @@
+const { test, expect } = require('@playwright/test');
+
+test.describe('Authentication & Authorization',  () =>{
+  test('Sign in with existing credentials', async ({page}) => {
+  await page.goto('https://coding.pasv.us/user/login')
+
+    await page.locator('#normal_login_email').fill('voitsekhivska.marina@gmail.com')
+    await page.locator('#normal_login_password').fill('*867542310*Vv')
+    await page.locator('button[type=\'submit\']').click()
+
+    await expect(page.locator('.ant-avatar-square')).toBeVisible()
+  })
+})
