33a75b378135b3d156fc38d08b46fea7fecd1569
marin <voitsekhivska.marina@gmail.com>
2023-09-05 15:07:41 +0200

    Test navigation

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -6,8 +6,20 @@ test.describe('Authentication & Authorization',  () =>{
 
     await page.locator('#normal_login_email').fill('voitsekhivska.marina@gmail.com')
     await page.locator('#normal_login_password').fill('*867542310*Vv')
-    await page.locator('button[type=\'submit\']').click()
+    await page.locator('button[type="submit"]').click()
 
     await expect(page.locator('.ant-avatar-square')).toBeVisible()
   })
+
+  test('Sign with in existing credentials', async ({page}) => {
+
+    await page.goto('https://coding.pasv.us/user/login')
+
+    await page.locator('#normal_login_email').fill('vitsekhivska.marina@gmail.com')
+    await page.locator('#normal_login_password').fill('*867542310*Vv')
+    await page.locator('button[type="submit"]').click()
+    const toast = page.locator('.ant-notification-notice-message')
+    await expect(toast).toBeVisible()
+    await expect(toast).toHaveText(' User login. Fail')
+    })
 })
