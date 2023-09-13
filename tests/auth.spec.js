22ecbe2f40b2b90ba586f5cd0d16fb6ea7b9aab5
marin <voitsekhivska.marina@gmail.com>
2023-09-13 16:03:43 +0200

    Add environment variables

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -2,13 +2,13 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('Authentication & Authorization',  () =>{
   test.beforeEach(async ({page}) => {
-    await page.goto('https://coding.pasv.us/user/login')
+    await page.goto('/user/login')
   })
 
   test('Sign in with existing credentials', async ({page}) => {
 
-    await page.locator('#normal_login_email').fill('voitsekhivska.marina@gmail.com')
-    await page.locator('#normal_login_password').fill('*867542310*Vv')
+    await page.locator('#normal_login_email').fill(process.env.EMAIL)
+    await page.locator('#normal_login_password').fill(process.env.PASSWORD)
     await page.locator('button[type="submit"]').click()
     await expect(page.locator('.ant-avatar-square')).toBeVisible()
   })
